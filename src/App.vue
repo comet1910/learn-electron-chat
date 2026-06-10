1ef58b3fe836b020b515956b673e2f04c2f99e67
Comet <supercomet1910@outlook.com>
2026-06-10 23:12:09 +0800

    phase1 主界面设计：对话列表ConversationList组件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
-  <h1 class="text-3xl font-bold underline">💖 Hello World!</h1>
-  <p>Welcome to your Electron application.</p>
+  <div class=" flex items-center justify-between h-screen">
+    <div class=" w-[300px] bg-gray-200 h-full border-r border-gray-300">
+      <ConversationList :items="items"/>
+    </div>
+    <div class="h-full flex-1">
+      content
+    </div>
+  </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { ConversationProps } from './types'
+import ConversationList from './components/ConversationList.vue';
+const items: ConversationProps[] = [
+  { id: 1, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用1', createdAt: '2024-07-03', updatedAt:'2024-07-03',providerId:1},
+  { id: 2, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用', createdAt: '2024-07-03', updatedAt:'2024-07-03',providerId:1},
+  { id: 3, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用', createdAt: '2024-07-03', updatedAt:'2024-07-03',providerId:1},
+  { id: 4, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用，你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt:'2024-07-03',providerId:1}
+]
+
 console.log('👋 This message is being logged by "App.vue", included via Vite');
 </script>
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+//存放数据结构
+export interface ConversationProps{
+    id: number;
+    title: string;
+    selectedModel: string;
+    createdAt: string;
+    updatedAt: string;
+    providerId: number;
+}
