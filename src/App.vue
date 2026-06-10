ad1b9f08bc6820bfbd270af2420ec2215990de9b
Comet <supercomet1910@outlook.com>
2026-06-11 01:19:59 +0800

    phase1 主界面设计：select模型选择

--- src/App.vue
+++ src/App.vue
@@ -24,16 +24,23 @@
         </button>
       </div>
     </div>
-    <div class="h-full flex-1">
-      content
+    <div class="h-full flex-1 flex items-center">
+     <div class="w-[80%] mx-auto">
+        {{selectedModel}}
+        <ProviderSelect :items="providers"  v-model="selectedModel" />
+     </div>
+    
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import {ref} from 'vue'
 import {Icon} from '@iconify/vue'
-import { ConversationProps } from './types'
+import { ConversationProps , ProviderProps } from './types'
 import ConversationList from './components/ConversationList.vue';
+import ProviderSelect from './components/ProviderSelect.vue';
+const selectedModel = ref('')
 const items: ConversationProps[] = [
   { id: 1, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用1', createdAt: '2024-07-03', updatedAt:'2024-07-03',providerId:1},
   { id: 2, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用', createdAt: '2024-07-03', updatedAt:'2024-07-03',providerId:1},
@@ -48,6 +55,28 @@ const items: ConversationProps[] = [
   { id: 11, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用', createdAt: '2024-07-03', updatedAt:'2024-07-03',providerId:1},
   { id: 12, selectedModel: 'GPT-3.5-Turbo', title: '什么是光合作用，你的说法很请正确，理解的很不错', createdAt: '2024-07-03', updatedAt:'2024-07-03',providerId:1},
 
+]
+
+const providers: ProviderProps[] = [
+    {
+        id: 1,
+        name: '文心一言',
+        desc: '文心一言 百度出品的大模型',
+        models: ['ERNIE-4.0-8K', 'ERNIE-3.5-8K', 'ERNIE-Speed-8K'],
+        avatar: 'https://qph.cf2.poecdn.net/main-thumb-pb-3004-50-jougqzjtwfqfyqprxbdwofvnwattmtrg.jp',
+        createdAt: '2024-07-03',
+        updatedAt: '2024-07-03'
+    },
+    {
+        id: 2,
+        name: '通义千问',
+        desc: '通义千问',
+        // https://help.aliyun.com/zh/dashscope/developer-reference/api-details?spm=a2c4g.11186623.0.
+        models: ['qwen-turbo', 'qwen-plus', 'qwen-max'],
+        avatar: 'https://qph.cf2.poecdn.net/main-thumb-pb-3004-50-jougqzjtwfqfyqprxbdwofvnwattmtrg.jp',
+        createdAt: '2024-07-03',
+        updatedAt: '2024-07-03'
+    }
 ]
 
 console.log('👋 This message is being logged by "App.vue", included via Vite');
