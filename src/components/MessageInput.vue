58f885c2ac17cafb14a0e3111420da699584e5e6
comet1910 <comet1910@163.com>
2026-06-25 22:33:47 +0800

    phase8 图片聊天功能：添加图片聊天功能

--- src/components/MessageInput.vue
+++ src/components/MessageInput.vue
@@ -1,30 +1,63 @@
 <template>
-  <div class="message-input w-full shadow-sm border rounded-lg border-gray-300 flex items-center">
-    <!-- 高亮部分 -->
-    <input class="outline-none border-0 flex-1 bg-white focus:ring-0" type="text" v-model="model" :disabled="disabled" />
-    <Button icon-name="radix-icons:paper-plane" @click="onCreate" :disabled="disabled">
-      发送
-    </Button>
+  <div class="message-input w-full shadow-sm border rounded-lg border-gray-300 py-1 px-2 focus-within:border-green-700">
+    <div v-if="imagePreview"  class="mb-2 relative flex items-center">
+      <img :src="imagePreview" alt="Preview" class="h-24 w-24 object-cover rounded">
+    </div>
+    <div class="flex items-center">
+      <input type="file"  accept="image/*" ref="fileInput" class="hidden" @change="handleImageUpload">
+      <Icon 
+        icon="radix-icons:image" 
+        width="24" 
+        height="24" 
+        :class="[
+          'mr-2',
+          disabled ? 'text-gray-300 cursor-not-allowed' : 'text-gray-400 cursor-pointer hover:text-gray-600'
+        ]"
+        @click="triggerFileInput"
+      />
+      <input class="outline-none border-0 flex-1 bg-white focus:ring-0" type="text" v-model="model" :disabled="disabled">
+      <Button icon-name="radix-icons:paper-plane" @click="onCreate" :disabled="disabled">
+        发送
+      </Button>
+    </div>
   </div>
 </template>
-
+  
 <script lang="ts" setup>
+import { ref } from 'vue'
+import { Icon } from '@iconify/vue'
 import Button from './Button.vue'
-
-defineProps<{
+const props = defineProps<{
   disabled?: boolean;
 }>()
-
 const emit = defineEmits<{
   create: [value: string]
 }>()
-
-// Vue 3.4+ 的响应式语法糖
 const model = defineModel<string>()
-
-const onCreate = () => {
-  if (model.value && model.value.trim() !== '') {
-    emit('create', model.value)
+const fileInput = ref<HTMLInputElement | null>(null)
+const imagePreview = ref('')
+const triggerFileInput = () => {
+  if (!props.disabled) {
+    fileInput.value?.click()
   }
 }
+const handleImageUpload = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  if (target.files && target.files.length > 0) {
+    console.log(target.files[0])
+    const selectedImage = target.files[0]
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      console.log(e.target?.result)
+      imagePreview.value = e.target?.result as string
+    }
+    reader.readAsDataURL(selectedImage)
+  }
+}
+const onCreate = () => {
+  if(model.value && model.value.trim() !== '') {
+    emit('create', model.value)
+  } 
+}
+
 </script>
